a9e6e8fa87d3bddcf41523b3c050e4ffb3659d05
aksharvijay <aksharvij@gmail.com>
2020-02-18 23:11:24 +1300

    product-commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Products from './components/Products';
+import { render } from '@testing-library/react';
 
-function App() {
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {products:[], filteredProducts:[], cartItems:[],};
+  }
+  componentWillMount(){
+    fetch("http://localhost:8000/products").then(res => res.json())
+    .then(data=>
+      this.setState({
+        products:data,filteredProducts:data
+      }));
+
+  }
+  render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Shopping Cart Application</h2>
+      <hr/>
+      <div class="row">
+        <div class="col-lg-8">
+          <Products products={this.state.filteredProducts} handleAddToCart={this.handleAddToCart}/>
+        </div>
+        <div class="col-lg-4">
+          <Cart />
+
+        </div>
+
+      </div>
+
     </div>
   );
+}
 }
 
 export default App;
---
+++ src/components/Products.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+export default class Products extends Component{
+    render(){
+        const productItems = this.props.products.map(product =>(
+            
+        ))
+        return (
+            <div>
+            
+            </div>
+        )
+    }
+}
